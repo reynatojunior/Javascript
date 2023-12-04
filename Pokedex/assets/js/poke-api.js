c674df55d4cfa58d90ed55b0a61a4a8f380369fc
Reynato Junior <45671354+reynatojunior@users.noreply.github.com>
2023-12-04 11:48:50 -0300

    commit listando pokemons

--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -3,8 +3,41 @@
 const pokeApi = {}
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+    function convertPokemonToHtml(pokemon){
+        return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+              <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+              </ol>
+
+              <img
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                alt="${pokemon.name}"
+              />
+            </div>
+          </li>
+        
+        `
+    }
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((pokemonList) => {
+            for (let i = 0; i < pokemonList.length; i++) {
+                const pokemon = pokemonList[i];
+                console.log(convertPokemonToLi(pokemon))
+                
+            }
+
+        })
+        
+        
+        //.catch((error) => console.error(error))
 }
