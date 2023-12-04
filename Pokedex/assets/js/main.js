aea431ae4ffffb739b4030320c455e7fb0680fe1
Reynato Junior <45671354+reynatojunior@users.noreply.github.com>
2023-12-04 20:37:58 -0300

    commit updade

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,15 +1,13 @@
-function convertPokemonTypesToLi(pokemon){
+function convertPokemonTypesToLi(pokemon) {
   return PokemonTypes.map((type) => {
     `
     
-    `
-  })
+    `;
+  });
 }
 
-
-
 function convertPokemonToLi(pokemon) {
-    return `
+  return `
     <li class="pokemon">
     <span class="number">${pokemon.order}</span>
     <span class="name">${pokemon.name}</span>
@@ -25,14 +23,12 @@ function convertPokemonToLi(pokemon) {
         alt="${pokemon.name}">
     </div>
   </li>
-  `
+  `;
 }
 
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-
-   const newHtml = pokemons.map(convertPokemonToLi).join('')
-   pokemonList.innerHTML = newHtml
-
-})
+  const newHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML = newHtml;
+});
--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -1,31 +1,28 @@
-const pokeApi = {}
+const pokeApi = {};
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-            .then((response) => response.json())
-}
-
+  return fetch(pokemon.url).then((response) => response.json());
+};
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+  const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-        return fetch(url)
-            .then((response) => response.json())
-            .then((jsonBody) => jsonBody.results)
-            .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-            .then((detailRequests) => Promise.all(detailRequests))
-            .then((pokemonsDetails) => {
-                debugger
-                console.log(pokemonsDetails)
-            })
-    
-}
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => {
+      debugger;
+      console.log(pokemonsDetails);
+    });
+};
 
 Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-]).then(results => {
-    console.log(results)
-})
+  fetch("https://pokeapi.co/api/v2/pokemon/1"),
+  fetch("https://pokeapi.co/api/v2/pokemon/2"),
+  fetch("https://pokeapi.co/api/v2/pokemon/3"),
+  fetch("https://pokeapi.co/api/v2/pokemon/4"),
+]).then((results) => {
+  console.log(results);
+});
