1eb5e6d352cf139584d49eacd0bb4b1e04434aa0
Reynato Junior <45671354+reynatojunior@users.noreply.github.com>
2023-12-04 16:11:11 -0300

    commit update

--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -6,7 +6,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
 }
 
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
         return fetch(url)
